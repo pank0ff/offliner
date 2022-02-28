47b1a92b6423c4890ea71a762dbece005e898f70
Admin <scorpion-18krek@mail.ru>
2022-02-28 13:56:22 +0300

    for add admin

--- src/main/java/com/example/offliner/controller/UserController.java
+++ src/main/java/com/example/offliner/controller/UserController.java
@@ -26,7 +26,7 @@ public class UserController {
     @Autowired
     private UserSevice userSevice;
 
-    @PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasAuthority('USER')")
     @GetMapping
     public String userList(Model model) {
         model.addAttribute("users", userRepo.findAll());
@@ -34,7 +34,7 @@ public class UserController {
         return "userList";
     }
 
-    @PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasAuthority('USER')")
     @GetMapping("{user}")
     public String userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
@@ -43,7 +43,7 @@ public class UserController {
         return "userEdit";
     }
 
-    @PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasAuthority('USER')")
     @PostMapping
     public String userSave(
             @RequestParam String username,
